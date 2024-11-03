0b0a80e1cd70a8b1a78b432cb1809da2b573133f
Francisco Manríquez <francisco.manriquezn@usm.cl>
2024-11-02 23:55:42 -0300

    Agregar TODO en SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,6 +2,7 @@ import { FaSearch } from "react-icons/fa"
 import "./SearchBar.css";
 
 export default function SearchBar() {
+  // TODO: terminar esto
 	return <div className="search-bar">
     <FaSearch className='icon'/>
     <input type="text" />
