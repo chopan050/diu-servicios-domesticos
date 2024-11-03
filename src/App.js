8489be1c0539eb26c202a4ef4e2cf1c124d22881
Esfirror <luis.letelier@outlook.cl>
2024-11-03 19:01:44 -0300

    Galeria y formulario de datos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import UserProfileForm from './UserProfileForm';
+import ImageGallery from './ImageGallery';
+import './styles.css';
+import profile1 from './assets/profile1.jpg';
+import profile2 from './assets/profile2.jpg';
+import profile3 from './assets/profile3.jpg';
+import profile4 from './assets/profile4.jpg';
+
+// Imágenes por defecto de la galería
+const defaultGalleryImages = [profile1, profile2, profile3, profile4];
 
 function App() {
+  const [profileImage, setProfileImage] = useState(defaultGalleryImages[0]);
+  const [showGallery, setShowGallery] = useState(false);
+
+  const [userData, setUserData] = useState({
+    firstName: 'Luis',
+    lastName: 'Godoy',
+    email: 'luis.godoy@gmail.com',
+    password: 'Contraseña123',
+    phone: '+569 3718 3112',
+    address: 'Av. Vicuña Mackenna 4608, Santiago',
+  });
+
+  const handleImageChange = (newImage) => {
+    setProfileImage(newImage); // Actualiza el estado con la nueva imagen
+  };
+  
+  const handleGallerySave = () => {
+    setShowGallery(false); // Cierra la galería y regresa al formulario
+  };
+  
+
+  const handleUserDataChange = (updatedData) => {
+    setUserData(updatedData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showGallery ? (
+        <div className="wrapper">
+          <button className="back-button" onClick={() => setShowGallery(false)}>&larr; Volver</button>
+          <ImageGallery
+            currentImage={profileImage}
+            images={defaultGalleryImages}
+            onImageChange={handleImageChange}
+            onSave={handleGallerySave}
+          />
+        </div>
+      ) : (
+        <div className="wrapper">
+          <button className="back-button">&larr; Volver</button>
+          <UserProfileForm
+            profileImage={profileImage}
+            userData={userData}
+            onUserDataChange={handleUserDataChange}
+            onEditImage={() => setShowGallery(true)}
+          />
+        </div>
+      )}
     </div>
   );
 }
